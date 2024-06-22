c94ecb01c107f9156816c591fdce5383863d50c3
Tushar Rohilla <tusharrohilla70#gmail.com>
2024-06-22 15:59:09 +0530

    revised

--- src/mongo_congo/mongo_crud.py
+++ src/mongo_congo/mongo_crud.py
@@ -1,25 +1,24 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from typing import Union
 
 
 class mongo_connect:
     def __init__(self, url: str, database_name: str, collection_name: str):
-        self.url = url
-        self.database_name = database_name
-        self.collection_name = collection_name
-        self.client = MongoClient(self.url)
+        self.url: str = url
+        self.database_name: str = database_name
+        self.collection_name: str = collection_name
+        self.client: MongoClient = MongoClient(self.url)  # Type annotation added here
         self.database = self.client[self.database_name]
         self.collection = self.database[self.collection_name]
 
-    def insert_record(self,record):
-        if type(record) == list:
+    def insert_record(self, record: Union[dict, list]):
+        if isinstance(record, list):
             for r in record:
-                if type(r) != dict:
-                    raise TypeError("Record must be a dictionary")
+                if not isinstance(r, dict):
+                    raise TypeError("Each element in the list must be a dictionary")
             self.collection.insert_many(record)
-
-        else:
-            if type(record) != dict:
-                raise TypeError("Record must be a dictionary")
+        elif isinstance(record, dict):
             self.collection.insert_one(record)
-        return 
+        else:
+            raise TypeError("Record must be a dictionary or a list of dictionaries")
